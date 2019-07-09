513067d3250f218a3d6dca0f151263046f7af483
Sébastien Cochet <SebastienSCochet@gmail.com>
2019-07-10 01:04:56 +0200

    Title and profil link structure +-ok Starting JobSeekComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfilLinkComponent } from './profil-link/profil-link.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MainTitleComponent } from './main-title/main-title.component';
+import { JobOffersComponent } from './job-offers/job-offers.component';
+import { LoginComponent } from './login/login.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { JobSeekComponent } from './job-seek/job-seek.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfilLinkComponent,
     NavigationComponent,
-    MainTitleComponent
+    MainTitleComponent,
+    JobOffersComponent,
+    LoginComponent,
+    HeaderComponent,
+    JobSeekComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: JobSeekComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'job-offers', component: JobOffersComponent }
+    ], { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
